defe8b315e1e04ba66616ebf63b18453e044a589
Edward <jaysongraphics@gmail.com>
2021-06-28 17:10:33 -0400

    commit tree 2d8114f06e90fb0c7d98c3386ea4ed3dda9be7c7

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
+
+
+
+
+
+# def square_array(array)
+#     array.each{|element| element ** 2}
+# end
+
 def square_array(array)
-  # your code here
+  array_square = []
+  array.each { |element| array_square << element ** 2 }
+  array_square
 end
